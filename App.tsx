af18802be131e59442b1f87af718e3c3a5400266
Eduardo Reichert <eduardo.reichert@totvs.com.br>
2024-10-02 08:34:37 -0300

    feat: add onesignal-expo-plugin

--- App.tsx
+++ App.tsx
@@ -8,6 +8,9 @@ import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+import { OneSignal } from 'react-native-onesignal';
+
+OneSignal.initialize("19ca260b-f57c-44b5-b51c-212ebfd7d112");
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
